004ec78524c5e2bc313815af370c1b2b36ba90f7
Alain Van Driessche <alainvd@gmail.com>
2021-06-25 10:37:31 +0200

    Logo

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,13 @@
-<div class="demo-spacing-content ecl-u-mv-xs"><span class="demo-spacing-item">&nbsp;</span></div>
+<header data-ecl-auto-init="SiteHeaderCore" class="ecl-site-header-core">
+    <div class="ecl-site-header-core__container ecl-container">
+        <div class="ecl-site-header-core__top">
 
+            <a class="ecl-link ecl-link--standalone ecl-site-header-core__logo-link"
+               href="/" aria-label="Home (European Commission)"><img alt="European Commission logo"
+                                                                            title="Home (European Commission)" style="width:240px"
+                                                                            src="{{asset('images/ECCC-logo.jpeg')}}" /></a>
 
+        </div>
+    </div>
 
-
+</header>
